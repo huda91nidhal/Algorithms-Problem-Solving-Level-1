c52ee36c8c9da1f0158cf99a7587f63d257cdf15
huda91nidhal <hudaalshomar@gmail.com>
2026-05-14 17:30:32 +0300

    Add files via upload

---
+++ #7.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int readnumber()
+{
+    int num;
+    cout << "please enter the number " << endl;
+    cin >> num;
+    return num;
+}
+float calculatehalfnum(int num)
+{
+    return float(num) / 2;
+}
+void printresult(int num)
+{
+    string result = "half of" + to_string(num) + " is " + to_string(calculatehalfnum(num));
+    cout << result << endl;
+}
+int main()
+{
+    printresult(readnumber());
+    return 0;
+}
+
