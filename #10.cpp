d8b91787084f29112788223ceb7afc922386cdc3
huda91nidhal <hudaalshomar@gmail.com>
2026-05-14 17:33:47 +0300

    Add files via upload

---
+++ #10.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+void readnumbers(int& num1, int& num2, int& num3)
+{
+    cout << "please enter number 1 : " << endl;
+    cin >> num1;
+    cout << "please enter number 2 : " << endl;
+    cin >> num2;
+    cout << "please enter number 3 : " << endl;
+    cin >> num3;
+
+}
+int sumof3numbers(int num1, int num2, int num3)
+{
+    return num1 + num2 + num3;
+}
+float calculateaverage(int num1, int num2, int num3)
+{
+    return (float)sumof3numbers(num1, num2, num3) / 3;
+}
+void printresult(float average)
+{
+    cout << "the average is : " << average << endl;
+}
+int main()
+{
+    int num1, num2, num3, sum;
+    readnumbers(num1, num2, num3);
+    printresult(calculateaverage(num1, num2, num3));
+    return 0;
+}
+
