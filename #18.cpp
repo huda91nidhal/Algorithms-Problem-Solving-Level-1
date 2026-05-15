6684c9dec86095ecfd9142441ca4dbed31ff33fb
huda91nidhal <hudaalshomar@gmail.com>
2026-05-15 10:54:47 +0300

    Add files via upload

---
+++ #18.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+float readnumber()
+{
+	float r;
+	cout << "please enter the value of r : " << endl;
+	cin >> r;
+	return r;
+}
+float circlearea(float r)
+{
+	const float pi = 3.14;
+	float area;
+	area = pi * pow(r, 2);
+	return area;
+}
+void printresult(float area)
+{
+	cout << "the area of the regtangle is :" << area;
+
+}
+int main()
+{
+	printresult(circlearea(readnumber()));
+	return 0;
+}
