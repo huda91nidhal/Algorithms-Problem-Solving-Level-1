10d7a5d962a1a8470552078dba966492936b219e
huda91nidhal <hudaalshomar@gmail.com>
2026-05-15 11:04:51 +0300

    Add files via upload

---
+++ #31.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int readnumber()
+{
+	int number;
+	cout << "please enter the number ? " << endl;
+	cin >> number;
+	return number;
+}
+void powerof2_3_4(int number)
+{
+	int a, b, c;
+	a = number * number;
+	b = number * number * number;
+	c = number * number * number * number;
+	cout << a << " " << b << " " << c << " " << endl;
+}
+int main()
+{
+	powerof2_3_4(readnumber());
+	return 0;
+}
