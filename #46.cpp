5c0406ae57b8543a3c35f4ee1de40481fa9eaaed
huda91nidhal <hudaalshomar@gmail.com>
2026-05-15 11:17:21 +0300

    Add files via upload

---
+++ #46.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+void printlattersAtoZ()
+{
+	for (int i = 65; i <= 90; i++)
+		cout << char(i) << endl;
+}
+int main()
+{
+	
+	printlattersAtoZ();
+	return 0;
+}
