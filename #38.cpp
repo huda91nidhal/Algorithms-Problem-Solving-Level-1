9ed324da3da739c54a94051d424c527e06a40fe5
huda91nidhal <hudaalshomar@gmail.com>
2026-05-15 11:10:58 +0300

    Add files via upload

---
+++ #38.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+enum enprimenotprime { prime = 1, notprime = 2 };
+float readpositivenumber(string message)
+{
+    int number = 0;
+    do
+    {
+        cout << message << endl;
+        cin >> number;
+    } while (number <= 0);
+    return number;
+}
+enprimenotprime checkprime(int number)
+{
+    int m = round(number / 2);
+    for (int counter = 2; counter <= m; counter++)
+    {
+        if (number % counter == 0)
+            return enprimenotprime::notprime;
+    }
+    return enprimenotprime::prime;
+}
+void printnumbertype(int number)
+{
+    switch (checkprime(number))
+    {
+    case enprimenotprime::prime:
+        cout << "the number is prime \n";
+        break;
+    case enprimenotprime::notprime:
+        cout << "the number is not prime \n";
+        break;
+    }
+}
+int main()
+{
+    printnumbertype(readpositivenumber("please enter a positive number"));
+    return 0;
+
+}
