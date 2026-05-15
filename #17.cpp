9a4da738ef18acf9d67432e139c50b04a36da6f2
huda91nidhal <hudaalshomar@gmail.com>
2026-05-15 10:53:40 +0300

    Add files via upload

---
+++ #17.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+void readnumbers(float& a, float& h)
+{
+	cout << "please enter the value of a : " << endl;
+	cin >> a;
+	cout << "please enter the value of h : " << endl;
+	cin >> h;
+
+}
+float tringlearea(float a, float h)
+{
+	float area = (a / 2) * h;
+	return area;
+}
+void printresult(float area)
+{
+	cout << "the area of the regtangle is :" << area;
+
+}
+int main()
+{
+	float a, h;
+	readnumbers(a, h);
+	printresult(tringlearea(a, h));
+	return 0;
+}
